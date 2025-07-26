fc8e9514955b8ef72beab853ef9b09e5485fbcf5
vivmuk <vivgates@hotmail.com>
2025-07-26 11:26:33 -0400

    Update Convex URL to production deployment

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 
-const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL || "https://cool-donkey-445.convex.cloud", {
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL || "https://cool-bulldog-876.convex.cloud", {
   unsavedChangesWarning: false,
 });
 
